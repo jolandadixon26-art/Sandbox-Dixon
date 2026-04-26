aebdee98c1c70ca507831c4518622d6fbb7c44bf
LSIS5830-2026Spring <jolandadixon26@gmail.com>
2026-04-26 17:12:09 -0400

    Add scripts.js with site information

---
+++ historic-areas/scripts.js
@@ -0,0 +1,4 @@
+const sites = [
+  { name: "Tryon Palace", location: "New Bern, Craven County, NC", image_url:"https://example.org/images/tryonpalace.jpg",
+    description:"Royal governor’s residence and state capitol building." ,
+    resources:["https://www.tryonpalace.org"] }
